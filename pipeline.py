dd6247db1cf1bc4ab5bfc733a5c7ebdb1239a4d6
pTarNav <p.tarragonavarra@gmail.com>
2024-11-20 14:02:59 +0100

    .

---
+++ pipeline.py
@@ -0,0 +1,8 @@
+
+class NegotiationDocumentToTranscriptMatching:
+
+	def __init__(self):
+		pass
+
+	def mention_tree_search(self):
+		pass
--- request_schemas.py
+++ request_schemas.py
@@ -1,19 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class NegotiationDocumentReference(BaseModel):
-	heading_numbering: str | None
-	paragraph_numbering: str | None
-	reference_text: str
+	heading_numbering: str | None = Field(description="")
+	paragraph_numbering: str | None = Field(description="")
+	reference_text: str = Field(description="Exact textual reference used to extract the heading numbering and the paragraph numbering. It is imperative that it is exactly the same text from the input.")
 
 
 class DirectMentionExtractor(BaseModel):
-	references: list[NegotiationDocumentReference]
+	references: list[NegotiationDocumentReference] = Field(description="")
 
 
 class IndirectMentionEvaluator(BaseModel):
 	contains_indirect_mention: bool
 
 
-class MentionTreeEvaluator(BaseModel):
+class MentionTreeSearchEvaluator(BaseModel):
 	is_relevant: bool
