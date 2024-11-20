039c8664d346d3138a9fc3a63ece0ed126141630
pTarNav <p.tarragonavarra@gmail.com>
2024-11-20 14:04:40 +0100

    .

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,9 @@
+from parser import NaiveDecisionParserDocument
 
 class NegotiationDocumentToTranscriptMatching:
 
-	def __init__(self):
+	def __init__(self, doc_content: NaiveDecisionParserDocument):
+		self.doc_content: NaiveDecisionParserDocument = doc_content
 		pass
 
 	def mention_tree_search(self):
